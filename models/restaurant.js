8586c3e0e19bde174ecab7c2a7fcf3f0c563af0c
five1star <five1star0420@gmail.com>
2020-11-13 15:01:17 +0900

    [Create] Redesign Tables

--- models/restaurant.js
+++ models/restaurant.js
@@ -3,31 +3,30 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Restaurant extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      models.User.hasMany(models.Matzip,{foreignKey: 'userId', sourceKey:'id'})
+      models.Restaurant.hasMany(models.Matzip,{foreignKey: 'restId', sourceKey:'id'})
       // define association here
     }
   };
-  User.init({
+  Restaurant.init({
     name: DataTypes.STRING,
-    mobile: DataTypes.STRING,
+    callNum: DataTypes.STRING,
     location: DataTypes.STRING,
-    imageRef: DataTypes.STRING,
-    nickname: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    isMatple: DataTypes.BOOLEAN,
-    isFirst: DataTypes.BOOLEAN,
-    isActive: DataTypes.BOOLEAN
+    photoRef: DataTypes.STRING,
+    priceLevel: DataTypes.INTEGER,
+    rating: DataTypes.INTEGER,
+    mainmenu: DataTypes.STRING,
+    visit: DataTypes.INTEGER,
+    like: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Restaurant',
   });
-  return User;
+  return Restaurant;
 };

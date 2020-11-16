14f0ebebf15b94df2d3b364ba5d50f654c599df5
QuePark <gyuhapark11@gmail.com>
2020-11-16 23:58:33 +0900

    [Add] a function for mypage with 'get' method closed #61

--- app.js
+++ app.js
@@ -20,16 +20,17 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 // Must use this when it is production
-// app.use(
-// 	cors({
-// 		origin: [
-// 			"http://ygm-here.s3-website.ap-northeast-2.amazonaws.com",
-// 			"http://18.223.115.35",
-// 		],
-// 		methods: ["GET", "POST", "PUT", "DELETE"],
-// 		credentials: true,
-// 	}),
-// );
+app.use(
+	cors({
+		origin: [
+			"http://ygm-here.s3-website.ap-northeast-2.amazonaws.com",
+			"http://18.223.115.35",
+			"http://localhost",
+		],
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	}),
+);
 
 app.use(
 	session({
